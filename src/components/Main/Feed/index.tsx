38b1ab28a8be00645aa1a5ef850c93adb9b3adda
Alerrando Breno <alerrando2@gmail.com>
2022-08-28 18:37:04 -0300

    main component feed for completed mobile

--- src/components/Main/Feed/index.tsx
+++ src/components/Main/Feed/index.tsx
@@ -1,6 +1,6 @@
 import './style.scss'
 import { storiesInfos } from '../Carrossel Stories';
-import { DotsThree } from 'phosphor-react';
+import { BookmarkSimple, ChatCircle, DotsThree, Heart, PaperPlaneTilt } from 'phosphor-react';
 
 export function Feed(){
     return (
@@ -15,6 +15,22 @@ export function Feed(){
                 </div>
                 <DotsThree size={32} weight="bold" />
             </header>
+
+            <main>
+                <div className='main-feed-img'>
+                    <img src="https://images.unsplash.com/photo-1660856514115-3e6d43e51021?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
+                </div>
+
+                <div className='main-feed-icons'>
+                    <div className='icons-interactions'>
+                        <Heart weight="bold" />
+                        <ChatCircle weight="bold" />
+                        <PaperPlaneTilt weight="bold" />
+                    </div>
+
+                    <BookmarkSimple weight="bold" />
+                </div>
+            </main>
         </div>
     )
 }
