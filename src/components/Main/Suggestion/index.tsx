cbf4dc5883cc3662ee03d04a92f119b5d2a81f01
Alerrando Breno <alerrando2@gmail.com>
2022-08-27 21:43:57 -0300

    completed the main of the info-container

--- src/components/Main/Suggestion/index.tsx
+++ src/components/Main/Suggestion/index.tsx
@@ -1,5 +1,37 @@
 import './style.scss';
 
+const suggestionPeople = [
+    {
+        url: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+        name: 'phote ????',
+        follow: "Suggestion for you",
+    },
+
+    {
+        url: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+        name: 'phote ????',
+        follow: "Followed by ????????",
+    },
+
+    {
+        url: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+        name: 'phote ????',
+        follow: "Suggestion for you",
+    },
+
+    {
+        url: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+        name: 'phote ????',
+        follow: "Suggestion for you",
+    },
+
+    {
+        url: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+        name: 'phote ????',
+        follow: "Suggestion for you",
+    }
+]
+
 export function Suggestion(){
     return(
         <main>
@@ -10,16 +42,20 @@ export function Suggestion(){
                 </header>
 
                 <main>
-                    <div className='suggestion-img'>
-                        <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" alt="" />
-                    </div>
-
-                    <div className='suggestion-info'>
-                        <p>Lorem ipsum dolor</p>
-                        <p>Lorem ipsum dolor sit amet consectetur</p>
-                    </div>
-
-                    <a href="#">Follow</a>
+                        {suggestionPeople.map((person, index) => (
+                            <div className='main-container-suggestion'>
+                                <div className='suggestion-img'>
+                                    <img src={suggestionPeople[index].url} alt={suggestionPeople[index].name} />
+                                </div>
+            
+                                <div className='suggestion-info'>
+                                    <p>Lorem ipsum dolor</p>
+                                    <p>{suggestionPeople[index].follow}</p>
+                                </div>
+            
+                                <a href="#">Follow</a>
+                            </div>
+                        ))}
                 </main>
 
                 <footer>
