64d3775d59e2f0bd67f38e34f591abf71d65ccab
Alerrando Breno <alerrando2@gmail.com>
2022-09-05 20:25:03 -0300

    add icons to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,15 @@
+import { MagnifyingGlass, House, MonitorPlay, Bag } from 'phosphor-react'
+import './style.scss';
+
 export function Footer(){
     return(
-        <footer>
-            <div>
-                
+        <footer className='footer-princ'>
+            <div className="container-princ-footer">
+                <House weight="bold" />
+                <MagnifyingGlass weight="bold" />
+                <MonitorPlay weight="bold" />
+                <Bag weight="bold" />
+                <House weight="bold" />
             </div>
         </footer>
     )
