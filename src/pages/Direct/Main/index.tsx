f45bfe71fda4ed20f0f35f1004adc7fa201211c3
Alerrando Breno <alerrando2@gmail.com>
2022-09-23 19:44:32 -0300

    direct online body

--- src/pages/Direct/Main/index.tsx
+++ src/pages/Direct/Main/index.tsx
@@ -1,13 +1,33 @@
 import { MagnifyingGlass } from "phosphor-react";
-import './style.scss';
+import { storiesInfos } from "../../../components/Main/Carrossel Stories";
+import "./style.scss";
 
 export function Main() {
   return (
     <main>
       <div className="search-main mobile">
         <div className="search">
-            <MagnifyingGlass size={24} weight="bold" />
-            <input type="text" name="search" id="search" placeholder="Search" />
+          <MagnifyingGlass size={24} weight="bold" />
+          <input type="text" name="search" id="search" placeholder="Search" />
+        </div>
+      </div>
+      <div className="stories-feed direct">
+        <div className="stories-container">
+          {storiesInfos.map((storie, index) => (
+            index !== 0 ? (
+              <div className="stories" key={index}>
+                <div className="stories-img">
+                  <img src={storiesInfos[index].url} alt="" />
+                </div>
+
+                <p translate="no" className="name-storie">
+                  {storiesInfos[index].name}
+                </p>
+              </div>
+            ) :(
+              null
+            )
+          ))}
         </div>
       </div>
     </main>
