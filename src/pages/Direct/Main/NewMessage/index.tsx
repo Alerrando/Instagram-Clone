ff0dc1516a92f29fcb69216d568e67871b5a3601
Alerrando Breno <alerrando2@gmail.com>
2022-09-28 10:17:29 -0300

    new-message header

--- src/pages/Direct/Main/NewMessage/index.tsx
+++ src/pages/Direct/Main/NewMessage/index.tsx
@@ -1,5 +1,20 @@
+import { ArrowLeft } from "phosphor-react";
+import './style.scss';
+
 export function NewMessage(){
     return(
-        <h1>Hello World</h1>
+        <>
+            <header className="newMessage-header">
+                <div className="container-header">
+                    <div className="title-newMessage">
+                        <ArrowLeft size={32} weight="bold" />
+
+                        <h2 className="h2">New Message</h2>
+                    </div>
+
+                    <span>Chat</span>
+                </div>
+            </header>
+        </>
     )
 }
