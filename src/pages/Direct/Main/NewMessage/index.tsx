c1ff921079518b59f0b4c8d130ec9bfd9306d6a7
Alerrando Breno <alerrando2@gmail.com>
2022-09-29 10:35:24 -0300

    add search of new message

--- src/pages/Direct/Main/NewMessage/index.tsx
+++ src/pages/Direct/Main/NewMessage/index.tsx
@@ -1,20 +1,57 @@
 import { ArrowLeft } from "phosphor-react";
-import './style.scss';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { storiesInfos } from "../../../../components/Main/Carrossel Stories";
+import "./style.scss";
 
-export function NewMessage(){
-    return(
-        <>
-            <header className="newMessage-header">
-                <div className="container-header">
-                    <div className="title-newMessage">
-                        <ArrowLeft size={32} weight="bold" />
+export function NewMessage() {
+    const [search, setSearch] = useState("");
 
-                        <h2 className="h2">New Message</h2>
-                    </div>
+    const procurar = storiesInfos.filter((storie) => Object.values(storie.name)
+    .join("")
+    .toLowerCase()
+    .includes(search.toLowerCase()))
 
-                    <span>Chat</span>
+  return (
+    <>
+      <header className="newMessage-header">
+        <div className="container-header">
+          <div className="title-newMessage">
+            <Link to="/direct">
+              <ArrowLeft size={32} weight="bold" />
+            </Link>
+
+            <h2 className="h2">New Message</h2>
+          </div>
+
+          <span>Chat</span>
+        </div>
+      </header>
+
+      <main>
+        <div className="search-new-message">
+          <label>Para</label>
+          <input type="text" name="search" id="search" placeholder="Search" onChange={e => setSearch((e.target as HTMLInputElement).value)} />
+        </div>
+
+        <div className="stories-container">
+          {procurar.map((storie, index) =>
+            procurar[index].name !== "My Story" ? (
+              <div className="stories" key={index}>
+                <div className="online"></div>
+                <div className="stories-img">
+                  <img src={procurar[index].url} alt="" />
                 </div>
-            </header>
-        </>
-    )
+
+                <p translate="no" className="name-storie">
+                  {procurar[index].name}
+                  <span>Online 14h</span>
+                </p>
+              </div>
+            ) : null
+          )}
+        </div>
+      </main>
+    </>
+  );
 }
