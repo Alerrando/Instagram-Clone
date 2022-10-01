3f357778e485c529d402dafa36eae31c9e946f72
Alerrando Breno <alerrando2@gmail.com>
2022-10-01 15:25:23 -0300

    arranging the modal header

--- src/pages/Direct/Main/Modal/index.tsx
+++ src/pages/Direct/Main/Modal/index.tsx
@@ -1,24 +1,26 @@
-import { ArrowLeft } from "phosphor-react";
-import '../NewMessage/style.scss'
+import { X } from "phosphor-react";
+import "../NewMessage/style.scss";
 import "./style.scss";
 
 type ModalProps = {
-    setModalMessage: (validation:boolean) => void
-}
+  setModalMessage: (validation: boolean) => void;
+};
 
-export function Modal(props:ModalProps) {
+export function Modal(props: ModalProps) {
   return (
     <div className="modal">
       <div className="modal-container">
         <header className="header-modal-header">
           <div className="container-header">
-            <div className="title-newMessage">
-                <ArrowLeft size={32} weight="bold" onClick={() => props.setModalMessage(false)} />
+            <X
+              size={22}
+              weight="bold"
+              onClick={() => props.setModalMessage(false)}
+            />
 
-              <h2 className="h2" translate="no">
-                New Message
-              </h2>
-            </div>
+            <h2 className="h2" translate="no">
+              New Message
+            </h2>
 
             <span translate="no">Chat</span>
           </div>
